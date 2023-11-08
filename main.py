2188c6965469373509de7ef4fbbf793cc6164a9e
RRA <rra1011@protonmail.com>
2023-11-08 12:52:59 -0800

    fix totw save logic

--- main.py
+++ main.py
@@ -14,15 +14,52 @@ default_timezone = 'America/Vancouver'
 
 
 @lru_cache
+def get_league(league_id):
+    response = requests.get(f'{api_host}/leagues', {'id': league_id})
+    return response.json()
+
+
+@lru_cache
+def get_league_totw_links(league_id):
+    league = get_league(league_id)
+    seasons = []
+    for item in league['stats']['seasonStatLinks']:
+        response = requests.get(item['TotwRoundsLink']).json()
+        seasons.append({'name': item['Name'], **response})
+    return seasons
+
+
+def save_league_totws(league_id):
+    totw_links = get_league_totw_links(league_id)
+    for item in totw_links:
+        season_name = str(item['name']).replace('/', '_')
+        season_name = season_name
+        for row in item['rounds']:
+            round_id = str(row['roundId']).replace('/', '_')
+            path = os.path.join('data/leagues', str(league_id), 'totw/seasons', season_name, 'rounds', round_id)
+            if not os.path.exists(path):
+                response = requests.get(row['link']).json()
+                os.makedirs(os.path.split(path)[0], exist_ok=True)
+                with open(path, 'w') as f:
+                    json.dump(response, f)
+
+
+
+
+
+
+
+
+
+
+
+@lru_cache
 def get_player(player_id):
     response = requests.get(f'{api_host}/playerData', {'id': player_id})
     return response.json()
 
 
-@lru_cache
-def get_league(league_id):
-    response = requests.get(f'{api_host}/leagues', {'id': league_id})
-    return response.json()
+
 
 
 def get_stat_series(player_id, *stat_names):
@@ -100,21 +137,21 @@ def get_total_appearances(player_id):
     return total
 
 
-def get_totw(league_id, season, round_id):
-    path = os.path.join('data/totw', league_id, season.replace('/', '_'), round_id.replace('/', '_'))
-    if not os.path.exists(path):
-        response = requests.get(f'{api_host}/team-of-the-week/team', params={
-            'leagueId': league_id, 'season': season, 'roundId': round_id
-        })
-        totw = response.json()
-        if totw:
-            os.makedirs(os.path.split(path)[0], exist_ok=True)
-            with open(path, 'w') as f:
-                json.dump(totw, f)
-        return totw
+# def get_totw(league_id, season, round_id):
+#     path = os.path.join('data/totw', league_id, season.replace('/', '_'), round_id.replace('/', '_'))
+#     if not os.path.exists(path):
+#         response = requests.get(f'{api_host}/team-of-the-week/team', params={
+#             'leagueId': league_id, 'season': season, 'roundId': round_id
+#         })
+#         totw = response.json()
+#         if totw:
+#             os.makedirs(os.path.split(path)[0], exist_ok=True)
+#             with open(path, 'w') as f:
+#                 json.dump(totw, f)
+#         return totw
     
-    with open(path) as f:
-        return json.load(f)
+#     with open(path) as f:
+#         return json.load(f)
     
 
 def get_fixtures(league_id, season):
